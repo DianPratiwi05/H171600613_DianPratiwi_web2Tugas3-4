862b029bd33f4ae818ac131b7660eda2e6c15a47
unknown <Dian Pratiwi>
2019-09-27 14:28:26 +0700

    tugas 3

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -75,19 +75,17 @@
                             </div>
                         </div>
 
-                         <div class="form-group row">
-                            <label for="captcha" class="col-md-4 col-form-label text-md-right">{{ __('CAPTCHA') }}</label>
+                         
+                        <div class="form-group row">
+                            <label for="captcha" class="col-md-4 col-form-label text-md-right">{{ __('Captcha') }}</label>
 
                             <div class="col-md-6">
-                                {!! Captcha::img() !!}
+                            <div class="captcha">
+                                <span>{!! captcha_img('flat') !!}</span>
+                                <button type="submit" class="btn btn-success btn-refresh" onclick="refreshCaptcha()">Refresh</button>
                             </div>
-                        </div>
 
-                         <div class="form-group row">
-                            <label for="captcha" class="col-md-4 col-form-label text-md-right">{{ __('captcha') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="captcha" type="captcha" class="form-control @error('captcha') is-invalid @enderror" name="captcha" required autocomplete="captcha">
+                                <input id="captcha" type="captcha" class="form-control mt-2 @error('captcha') is-invalid @enderror" name="captcha" placeholder="Enter Captcha">
 
                                 @error('captcha')
                                     <span class="invalid-feedback" role="alert">
@@ -96,7 +94,6 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
